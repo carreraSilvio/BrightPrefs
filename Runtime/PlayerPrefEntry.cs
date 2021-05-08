2ed9102cc6dc819cf81c818c866aaf4e81060875
Silvio Carrera <carrera.silvio@gmail.com>
2021-05-08 16:16:43 -0300

    Coded PlayerPrefEntry

---
+++ Runtime/PlayerPrefEntry.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace BrightPrefs.Runtime
+{
+    public class PlayerPrefEntry<T> 
+    {
+        public T Value 
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                var valueJson = JsonUtility.ToJson(value);
+                PlayerPrefs.SetString(_key, valueJson);
+            }
+        }
+
+        private T _value;
+        private readonly string _key;
+
+        public PlayerPrefEntry(string key)
+        {
+            _key = key;
+        }
+
+        public void Save()
+        {
+            PlayerPrefs.Save();
+        }
+    }
+}
