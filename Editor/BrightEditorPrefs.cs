c6de46fdf00417023eb9b68cdbd45eecc2cd7f8f
Silvio Carrera <carrera.silvio@gmail.com>
2021-05-08 16:51:31 -0300

    Created EditorPrefs extensions

---
+++ Editor/BrightEditorPrefs.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BrightPrefs.Editortime
+{
+    /// <summary>
+    /// Wrapper for UnityEditor.EditorPrefs with object support.
+    /// </summary>
+    public class BrightEditorPrefs
+    {
+        public static void SetObject(string key, object value)
+        {
+            SetString(key, JsonUtility.ToJson(value));
+        }
+
+        public static object GetObject<T>(string key, object defaultValue = default)
+        {
+            var stringValue = GetString(key, string.Empty);
+            return string.IsNullOrEmpty(stringValue) ?
+                defaultValue :
+                JsonUtility.FromJson<T>(stringValue);
+        }
+
+        #region Wrapper Methods
+        public static void DeleteAll() => EditorPrefs.DeleteAll();
+        public static void DeleteKey(string key) => EditorPrefs.DeleteKey(key);
+
+        public static bool GetBool(string key, bool defaultValue = false)
+            => EditorPrefs.GetBool(key, defaultValue);
+        public static float GetFloat(string key, float defaultValue = 0f)
+            => EditorPrefs.GetFloat(key, defaultValue);
+        public static int GetInt(string key, int defaultValue = 0)
+            => EditorPrefs.GetInt(key, defaultValue);
+        public static string GetString(string key, string defaultValue = "")
+            => EditorPrefs.GetString(key, defaultValue);
+
+        public static bool HasKey(string key)
+            => EditorPrefs.HasKey(key);
+
+
+        public static void SetBool(string key, bool value)
+            => EditorPrefs.SetBool(key, value);
+        public static void SetFloat(string key, float value)
+            => EditorPrefs.SetFloat(key, value);
+        public static void SetInt(string key, int value)
+            => EditorPrefs.SetInt(key, value);
+        public static void SetString(string key, string value)
+            => EditorPrefs.SetString(key, value); 
+        #endregion
+    }
+}
---
+++ Editor/EditorPrefEntry.cs
@@ -0,0 +1,29 @@
+﻿namespace BrightPrefs.Editortime
+{
+    /// <summary>
+    /// Maintains key and value of a EditorPref
+    /// </summary>
+    public sealed class EditorPrefEntry<T> : BrightEditorPrefs
+    {
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                SetObject(_key, value);
+            }
+        }
+
+        private T _value;
+        private readonly string _key;
+
+        public EditorPrefEntry(string key)
+        {
+            _key = key;
+        }
+    }
+}
